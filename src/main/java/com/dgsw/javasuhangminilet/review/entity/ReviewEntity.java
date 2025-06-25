d8ac30d9784e9779bccbb824f88235f97222876b
dlrjsgml44 <leegeh1213@gmail.com>
2025-06-25 17:48:52 +0900

    조인

--- src/main/java/com/dgsw/javasuhangminilet/review/entity/ReviewEntity.java
+++ src/main/java/com/dgsw/javasuhangminilet/review/entity/ReviewEntity.java
@@ -1,18 +1,25 @@
 package com.dgsw.javasuhangminilet.review.entity;
 
+import com.dgsw.javasuhangminilet.auth.entity.UserEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @Getter
 @Setter
-@Table(name="reviews")
+@Builder
+@Table(name = "reviews")
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동으로 ID 증가
     private Long id;
-
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private UserEntity user;
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String content;
+
 }
